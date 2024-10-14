30edcf77a10a21c0a73ed416ecff9b950135da34
Hadayetullah <hadayetullah30@gmail.com>
2024-10-14 20:13:41 +0600

    Create an app app_product

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',  # Add the blacklist app here
     'corsheaders',
     'app_useraccount',
+    'app_product'
 ]
 
 MIDDLEWARE = [
